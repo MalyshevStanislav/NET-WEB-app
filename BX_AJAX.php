fd7d84efa48136ec395a1f5599f3636609eb3d1e
Stas Malyshev <malishev.stanislav@yandex.ru>
2024-11-12 18:10:23 +0500

    aroad2 BX_AJAX.php

--- BX_AJAX.php
+++ BX_AJAX.php
@@ -5,15 +5,15 @@ require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 //устанавливаем заголовок страницы
 $APPLICATION->SetTitle("AJAX");
 
-   CJSCore::Init(array('ajax'));
-   $sidAjax = 'testAjax';
-if(isset($_REQUEST['ajax_form']) && $_REQUEST['ajax_form'] == $sidAjax){
-   $GLOBALS['APPLICATION']->RestartBuffer();
-   echo CUtil::PhpToJSObject(array(
+   CJSCore::Init(array('ajax')); /*подключаем библиотеку JS битрикс*/
+   $sidAjax = 'testAjax'; /*проверка ajax-запроса*/
+if(isset($_REQUEST['ajax_form']) && $_REQUEST['ajax_form'] == $sidAjax){ /*проверяем передан ли параметр ajax_form и сравниваем с $sidAjax*/
+   $GLOBALS['APPLICATION']->RestartBuffer(); /*очищаем буфер обмена*/
+   echo CUtil::PhpToJSObject(array( /*преобразуем массив в объект с данными для ответа пользователю*/ 
             'RESULT' => 'HELLO',
             'ERROR' => ''
    ));
-   die();
+   die();/*завершаем спринт*/ 
 }
 
 ?>
@@ -26,7 +26,7 @@ if(isset($_REQUEST['ajax_form']) && $_REQUEST['ajax_form'] == $sidAjax){
 <script>
    window.BXDEBUG = true; /*включаем режим отладки для библиотеки BX */
 function DEMOLoad(){
-   BX.hide(BX("block")); /*скрываем блок*/ */
+   BX.hide(BX("block")); /*скрываем блок*/ 
    BX.show(BX("process")); /*показываем элемент загрузки*/
    /*выполняется ajax-запрос к странице с формой*/
    BX.ajax.loadJSON(
@@ -35,14 +35,14 @@ function DEMOLoad(){
    );
 }
 function DEMOResponse (data){
-   BX.debug('AJAX-DEMOResponse ', data); /*выводим данные с типом дата*/ */
-   BX("block").innerHTML = data.RESULT; /*заполняем блок данными из запроса*/ */
-   BX.show(BX("block")); /*показываем блок с данными*/ */
-   BX.hide(BX("process"));/*скрываем элемент загрузки*/ */
+   BX.debug('AJAX-DEMOResponse ', data); /*выводим данные с типом дата*/
+   BX("block").innerHTML = data.RESULT; /*заполняем блок данными из запроса*/
+   BX.show(BX("block")); /*показываем блок с данными*/
+   BX.hide(BX("process"));/*скрываем элемент загрузки*/
 
-   BX.onCustomEvent( /*генерация пользовательского события*/ */
-      BX(BX("block")),/*получаем ссылку на элемент блока*/ */
-      'DEMOUpdate' /*индентификатор события для элемента с типом block*/ */
+   BX.onCustomEvent( /*генерация пользовательского события*/ 
+      BX(BX("block")),/*получаем ссылку на элемент блока*/ 
+      'DEMOUpdate' /*индентификатор события для элемента с типом block*/ 
    );
 }
 
@@ -52,24 +52,24 @@ BX.ready(function(){
       window.location.href = window.location.href;
    });
    */
-   BX.hide(BX("block")); /*скрываем элемент "block"*/ */
-   BX.hide(BX("process")); /*скрываем элемент с загрузкой*/ */
+   BX.hide(BX("block")); /*скрываем элемент "block"*/ 
+   BX.hide(BX("process")); /*скрываем элемент с загрузкой*/ 
    /*делегируем обработку события на родительский элемент в document.body*/
-    BX.bindDelegate(  */ /*вызываем функцию делегирования события*/ */
-      document.body, 'click', {className: 'css_ajax' }, /*в родительском документе по клику на элемент с классом css_ajax будет запущена функция*/ */
-      function(e){ /*объявляем функцию*/ */
+    BX.bindDelegate(   /*вызываем функцию делегирования события*/ 
+      document.body, 'click', {className: 'css_ajax' }, /*в родительском документе по клику на элемент с классом css_ajax будет запущена функция*/
+      function(e){ /*объявляем функцию*/ 
          if(!e)
-            e = window.event; /*обеспечиваем совместимость с браузерами*/ */
+            e = window.event; /*обеспечиваем совместимость с браузерами*/ 
          
-         DEMOLoad(); /*вызываем функцию DEMOLoad()*/ */
-         return BX.PreventDefault(e); /*предотвращаем выполнения действия по умолчанию*/ */
+         DEMOLoad(); /*вызываем функцию DEMOLoad()*/ 
+         return BX.PreventDefault(e); /*предотвращаем выполнения действия по умолчанию*/ 
       }
    );
    
 });
 
 </script>
-<div class="css_ajax">click Me</div> /*создаем элемент, по клику на который будет вызываться функция DEMOLoad*/ */
+<div class="css_ajax">click Me</div> /*создаем элемент, по клику на который будет вызываться функция DEMOLoad*/
 <?
 //подключаем эпилог ядра bitrix
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
